19df648902aa06ff28def02ba5d13cd7a596447c
rafaelMatos <rafael-matos@outlook.com>
2026-06-25 09:03:02 -0300

    style: prettie changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,34 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals",'next/typescript', 'prettier'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     ignores: [
       'src/generated/prisma/**',
       'pgdata/**',
       '.next/**',
       'node_modules/**',
-    ]
+    ],
   },
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     rules: {
-      'arrow-body-style':'off',
-      'prefer-arrow-callback':'off',
-    }
-  }
-]
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
+];
 
 export default eslintConfig;
